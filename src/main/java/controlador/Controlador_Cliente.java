3ebcbc12e53cc46556c456e0bbbc6773414b3337
BilmanAndres <148238669+BilmanAndres@users.noreply.github.com>
2023-10-18 20:09:35 -0500

    Update Controlador_Cliente.java

--- src/main/java/controlador/Controlador_Cliente.java
+++ src/main/java/controlador/Controlador_Cliente.java
@@ -5,6 +5,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.sql.Date;
 import java.util.Map;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
@@ -72,6 +73,18 @@ public class Controlador_Cliente implements ActionListener {
             JOptionPane.showMessageDialog(null, "!!EXITO!!");
             String valorSexo = cli.getjCoboxsexo().getSelectedItem().toString();
             int sexo = mode_cli.llenarCombo("sexo").get(valorSexo);
+
+            java.util.Date fec = cli.getjDateChooser1().getDate();
+            long fe = fec.getTime();
+            java.sql.Date fecha = new Date (fe);
+            
+            mode_cli.setIdClien(Integer.parseInt(cli.getjTextcilente().getText()));
+            mode_cli.setNom(cli.getjTextnombre().getText());
+            mode_cli.setTel(cli.getjTextelefono().getText());
+            mode_cli.setCorr(cli.getjTextcorreo().getText());
+            mode_cli.setIdsex(sexo);
+            mode_cli.setFech_naci(fecha);
+            
 
         }
         }
